5e6d4e7c79370841bafe7f0c037d97ee5d0b8f4a
tayjesse83 <tayjesse83@gmail.com>
2026-07-17 04:32:19 +0000

    Add files via upload

---
+++ _app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from ui import dashboard, drilling_page, cement_page
+
+# Set page structure
+st.set_page_config(
+    page_title="PyMudCement-Optima",
+    page_icon="💧",
+    layout="wide"
+)
+
+# Sidebar Routing Navigation
+st.sidebar.title("Navigation")
+page = st.sidebar.radio(
+    "Go To:",
+    ["Main Dashboard", "Drilling Fluids & Hydraulics", "Cementing Module"]
+)
+
+# Page Router
+if page == "Main Dashboard":
+    dashboard.show_page()
+elif page == "Drilling Fluids & Hydraulics":
+    drilling_page.show_page()
+elif page == "Cementing Module":
+    cement_page.show_page()
